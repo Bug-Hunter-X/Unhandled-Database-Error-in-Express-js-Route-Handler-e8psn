6e1a316155612fe1126e5cfd67db790980fe068b
BugHunter-X <askzenai@gmail.com>
2025-02-19 18:18:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // Missing error handling for invalid userId
+  db.getUser(userId, (err, user) => {
+    if (err) {
+      // ... handle error, but we will simulate the missing handling
+      res.status(500).send('Something broke!');
+    } else {
+      res.json(user);
+    }
+  });
+});
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
